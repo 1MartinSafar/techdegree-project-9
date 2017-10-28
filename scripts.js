8f51dd12f82cecc17704d15416529e8e0520e5f1
Martin Safar <1martinsafar@gmail.com>
2017-10-28 10:31:34 +0200

    Alert Notification almost ready

---
+++ scripts.js
@@ -0,0 +1,24 @@
+// Hide the Alert Notification on click
+
+// const alert = document.querySelector(".alert-div");
+// const alert_x = document.querySelector(".alert-div p::after");
+//
+// alert.innerHTML = "
+//   content: "X";
+//
+// ";
+//
+// console.log(alert);
+// console.log(alert_x);
+
+
+// .alert-div p::after {
+//   content: "X";
+//   font-size: 16px;
+//   font-weight: bold;
+//   position: relative;
+//   right: 0%;
+//
+//   align-self: center;
+//   margin-left: auto;
+// }
