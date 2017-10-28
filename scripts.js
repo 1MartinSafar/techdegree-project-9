25b35d975d65b393194420a82ed6f372eb116e5f
Martin Safar <1martinsafar@gmail.com>
2017-10-28 11:02:12 +0200

    Alert Notification modified and finished

--- scripts.js
+++ scripts.js
@@ -1,24 +1,11 @@
 // Hide the Alert Notification on click
 
-// const alert = document.querySelector(".alert-div");
-// const alert_x = document.querySelector(".alert-div p::after");
-//
-// alert.innerHTML = "
-//   content: "X";
-//
-// ";
-//
-// console.log(alert);
-// console.log(alert_x);
+const alert = document.querySelector(".alert");
+const alert_close = document.querySelector(".alert-close");
 
+console.log(alert);
+console.log(alert_close);
 
-// .alert-div p::after {
-//   content: "X";
-//   font-size: 16px;
-//   font-weight: bold;
-//   position: relative;
-//   right: 0%;
-//
-//   align-self: center;
-//   margin-left: auto;
-// }
+alert_close.addEventListener("click", function() {
+  alert.style.display = "none";
+});
